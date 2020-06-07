d280d5265c1204d396eab5b442fa2587c1b5b736
isaac israel <email.iisrael@gmail.com>
2020-06-07 20:34:55 -0300

    Create Registrations Controller with Token

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    devise_for  :users,
+                skip: [
+                  :passwords,
+                  # :registrations,
+                  :sessions,
+                  :confirmations],
+                path: '',
+                path_names: {
+                  registration: 'signup'
+                },
+                controllers: {
+                  registrations: 'registrations'
+                }
+  end
 end
